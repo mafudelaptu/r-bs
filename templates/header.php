55fb7af4e05b2eb885d065ebafc2bcc7bc0fc5b2
mafudelaptu <mafudelaptu@gmail.com>
2014-06-08 18:47:17 +0200

    init commit

---
+++ templates/header.php
@@ -0,0 +1,15 @@
+<header class="banner container" role="banner">
+	<div class="row">
+		<div class="col-sm-2">
+			<?php dynamic_sidebar('sidebar-language-select'); ?>
+		</div>
+		<div class="col-sm-10 text-right">
+			
+			<a class="brand" href="<?php echo home_url('/') ?>" title="<?php bloginfo('name'); ?>">
+				<img src="<?php echo child_template_directory; ?>/assets/img/hbs_logo.gif" alt="<?php bloginfo('name'); ?> Logo" class="brand_logo">
+			</a>
+
+		</div>
+	</div>
+	
+</header>
