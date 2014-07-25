7518c9afc206eae68adbbd6bcb2982c4ce39c54a
mafudelaptu <mafudelaptu@gmail.com>
2014-07-25 18:01:10 +0200

    fixes

--- wp-content/themes/r-bs-theme/templates/header.php
+++ wp-content/themes/r-bs-theme/templates/header.php
@@ -1,7 +1,7 @@
 <header class="banner container pl0 pr0" role="banner">
 	<div class="text-right">
 			<a class="brand" href="<?php echo home_url('/') ?>" title="<?php bloginfo('name'); ?>">
-				<img src="<?php echo child_template_directory; ?>/assets/img/RBS_Logo.gif" alt="<?php bloginfo('name'); ?> Logo" class="brand_logo">
+				<img src="<?php echo child_template_directory; ?>/assets/img/LogoRBS.gif" alt="<?php bloginfo('name'); ?> Logo" class="brand_logo">
 			</a>
 	</div>
 	<div class="">
